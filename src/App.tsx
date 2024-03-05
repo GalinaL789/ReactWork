d3f0dc28ded162f81e1ea851758c101149803a73
Galina Lasko <galina_lasko@hotmail.com>
2024-03-05 16:29:57 +0100

    added sceleton for my work

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+
+// import Homework20 from "./homeworks/Homework20/Homework20";
+import GlobalStyles from "styles/GlobalStyles";
+import Homework22 from "homeworks/Homework22/Homework22";
+import Lesson23 from "lessons/Lesson23/Lesson23";
+import Homework23 from "homeworks/Homework23/Homework23";
+import Lesson24 from "lessons/Lesson24/Lesson24";
+import Homework24 from "homeworks/Homework24/Homework24";
+import Lesson25 from "lessons/Lesson25/Lesson25";
+
+
+function App() {
+  return ( 
+    <>
+      <GlobalStyles/>
+      {/* <Lesson20 /> */}
+      {/* <Lesson21 /> */}
+      {/* <Homework22 /> */}
+      {/* <Homework23/> */}
+      {/* <Homework24/> */}
+      <Lesson25/>
+      {/* <Homework20 /> */}
+      {/* <Homework21 /> */}
+      </>
+  );
+}
+export default App;
